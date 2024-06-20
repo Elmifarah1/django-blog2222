01ddc93a0378515dfcd65f6bcd576e6a72e2b003
Elmi Farah <elmifarah98@gmail.com>
2024-06-20 11:00:05 +0000

    implemented model defenition from gerd and added it into code

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-elmifarah1-djangoblog22-o7uaa1i618q.ws-eu114.gitpod.io", 
                  ".herokuapp.com'"]
