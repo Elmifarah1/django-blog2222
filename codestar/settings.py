dd12b08bd9335b42c4f2d1711de5099171ebdc42
Elmi Farah <elmifarah98@gmail.com>
2024-06-20 13:36:43 +0000

    added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
         '8000-elmifarah1-djangoblog22-o7uaa1i618q.ws-eu114.gitpod.io', '.herokuapp.coms']
