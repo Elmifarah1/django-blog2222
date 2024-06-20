71e463c344fe30762b3b7bb997e480cfdfd7d3e0
Elmi Farah <elmifarah98@gmail.com>
2024-06-20 10:25:20 +0000

    I have created a post model and have also connected my database

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^!8ac67w14)*e&2)-oac5pq^_7i-dr@5ymvl3ze95896ujy2m8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-elmifarah1-djangoblog22-o7uaa1i618q.ws-eu114.gitpod.io', 
-                '.herokuapp.com']
+ALLOWED_HOSTS = ["8000-elmifarah1-djangoblog22-o7uaa1i618q.ws-eu114.gitpod.io", 
+                 ".herokuapp.com'"]
 
 
 # Application definition
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-elmifarah1-djangoblog22-o7uaa1i618q.ws-eu114.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
